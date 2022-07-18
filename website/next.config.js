d8b3cbacbea5ba8f98fd8767f35b6a7a82c0f8f8
Raí B. Toffoletto <rai@toffoletto.me>
2022-07-18 07:29:23 +0100

    dev: project structure update

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const isProduction = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: false,
+
+  swcMinify: true,
+
+  productionBrowserSourceMaps: true,
+
+  compiler: {
+    emotion: true,
+    styledComponent: true,
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
+
+  rewrites: !isProduction
+    ? () => [
+        {
+          source: '/@:route',
+          destination: `${process.env.PREVIEW_PREFIX ?? '/'}:route`,
+        },
+      ]
+    : undefined,
+};
+
+module.exports = nextConfig;
