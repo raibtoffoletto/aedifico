73cfb81937de469f1a30236956e8eff6bc453b32
Raí B. Toffoletto <rai@toffoletto.me>
2022-07-30 10:20:47 +0100

    dev: implement layout

--- sprintplank.dev/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sprintplank</h1>
-      </header>
-    </div>
-  );
-}
--- sprintplank.dev/src/index.jsx
+++ sprintplank.dev/src/index.jsx
@@ -1,5 +1,45 @@
+import './styles.css';
+import '@fontsource/ibm-plex-mono/100.css';
+import '@fontsource/ibm-plex-mono/100-italic.css';
+import '@fontsource/ibm-plex-mono/200.css';
+import '@fontsource/ibm-plex-mono/200-italic.css';
+import '@fontsource/ibm-plex-mono/300.css';
+import '@fontsource/ibm-plex-mono/300-italic.css';
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/400-italic.css';
+import '@fontsource/ibm-plex-mono/500.css';
+import '@fontsource/ibm-plex-mono/500-italic.css';
+import '@fontsource/ibm-plex-mono/600.css';
+import '@fontsource/ibm-plex-mono/600-italic.css';
+import '@fontsource/ibm-plex-mono/700.css';
+import '@fontsource/ibm-plex-mono/700-italic.css';
+import '@fontsource/nunito/200.css';
+import '@fontsource/nunito/300.css';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/500.css';
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/nunito/900.css';
+import '@fontsource/ubuntu/300.css';
+import '@fontsource/ubuntu/300-italic.css';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/400-italic.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/500-italic.css';
+import '@fontsource/ubuntu/700.css';
+import '@fontsource/ubuntu/700-italic.css';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme as appTheme } from './lib';
+import { Layout } from './components/Layout';
+import Router from './pages/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ChakraProvider theme={appTheme}>
+    <Layout>
+      <Router />
+    </Layout>
+  </ChakraProvider>
+);
